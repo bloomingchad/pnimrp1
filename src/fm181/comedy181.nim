4c1df25d397f4f0516dca12ea4980353ba02fa7c
antonl05 <88133270+antonl05@users.noreply.github.com>
2021-09-01 16:40:31 +0530

    Create comedy181.nim

---
+++ src/fm181/comedy181.nim
@@ -0,0 +1,36 @@
+proc comedy181() =
+  var sect:string = "Comedy"
+  var f = readFile("pnimrp.d/181FM/comedy181.csv").split('\n')
+  proc s() =
+    back(17)
+    echo "PNimRP -> ",sect
+    echo ""
+    echo "Stations Playing ",sect," Music:"
+    echo "1 ", f[0]
+    echo "2 ", f[1]
+    echo "3 ", f[2]
+    echo "R Return"
+    echo "Q Quit"
+  proc r() =
+    while true:
+      sleep 160
+      var key = getKey()
+      case key:
+        of Key.None: discard
+        of Key.One:
+          call(sub,sect,f[0],f[3])
+          s() ; r()
+        of Key.Two:
+          call(sub,sect,f[1],f[4])
+          s() ; r()
+        of Key.Three:
+          call(sub,sect,f[2],f[5])
+          s() ; r()
+        of Key.R: fm181()
+        of Key.Escape , Key.Q: exitEcho() ;exitProc()
+        else:
+          inv()
+          r()
+    tb.display()
+    sleep(20)
+  s() ; r()
