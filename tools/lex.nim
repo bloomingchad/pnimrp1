5456432781274756e8a0ddb4e4c3d401f51252b3
antonl05 <antonloyed007@gmail.com>
2021-09-16 22:31:16 +0530

    improve lex

--- tools/lex.nim
+++ tools/lex.nim
@@ -1,24 +1,39 @@
 #tool to make parsing values from pmrp easier
 
-from strutils import splitLines,removePrefix,removeSuffix,delete,contains,join
+from strutils import splitLines,removeSuffix,delete,contains,join,strip,find
 from os import commandLineParams,copyFile
 from strformat import fmt
 
 let lex = commandLineParams()
-
 if lex == @[]: echo "give files" ; quit 1
+
+proc isEven(x:int):bool = x mod 2 == 0
 
 for f in lex.low..lex.high:
  var file = lex[f]
  copyFile(file,fmt"{file}.bkp")
  var inSeq = readFile(file).splitLines()
+
  for f in inSeq.low..inSeq.high:
-  if inSeq[f] == "": echo fmt"warning: {file} {f + 1}: line is nil"
+  if inSeq[f] == "": echo fmt"info: {file} {f + 1}: line is nil"
+  elif inSeq[f].contains "radionomy":
+   inSeq[f] = ""
+   #if f
+   #inSeq[(f/2).int] = ""
+   echo fmt"info: {file} {f + 1}: line had radionomy link"
+   
   elif inSeq[f].contains " ":
-   #inSeq[f] = ""
-   echo fmt"warning: {file} {f + 1}: line has space"
+   echo fmt"info: {file} {f + 1}: line had space"
+   inSeq[f] = inSeq[f].strip()
+   try:
+    removeSuffix(inSeq[f],"\"")
+    inSeq[f].delete(inSeq.low..(inSeq[f].find("=") + 1))
+   except: discard
+
   else:
-   removePrefix(inSeq[f],"\"")
+   inSeq[f] = inSeq[f].strip()
    removeSuffix(inSeq[f],"\"")
-   inSeq[f].delete(0..7)
- writeFile(file,inSeq.join("\n"))
+   inSeq[f].delete(inSeq.low..(inSeq[f].find("=") + 1))
+
+ echo inSeq.join("\n")
+ #writeFile(file,inSeq.join("\n"))
