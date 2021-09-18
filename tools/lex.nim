78b5086362f61b2cf7bf6f96f9d8354278087a0a
antonl05 <antonloyed007@gmail.com>
2021-09-18 22:16:20 +0530

    lex improvements

--- tools/lex.nim
+++ tools/lex.nim
@@ -1,44 +1,31 @@
 #tool to make parsing values from pmrp easier
-
-from strutils import splitLines,removePrefix,removeSuffix,contains,join,strip,find,delete,replace
-from os import commandLineParams,copyFile
+from strutils import splitLines,removeSuffix,contains,join,strip,find,delete
+from os import commandLineParams,copyFile,fileExists
 from strformat import fmt
 
 let lex = commandLineParams()
 if lex == @[]: echo "give files" ; quit 1
 
-proc isEven(x:int):bool = x mod 2 == 0
-
 for f in lex.low..lex.high:
- var file = lex[f]
- copyFile(file,fmt"{file}.bkp")
- var inSeq = readFile(file).splitLines()
-
- for f in inSeq.low..inSeq.high:
-  if inSeq[f] == "": echo fmt"info: {file} {f + 1}: line is nil"
-  elif inSeq[f].contains "onomy":
-   inSeq[f] = ""
-   var e = ( inSeq.high / 2 ).int - ( inSeq.high - f ).int 
-   inSeq[e] = ""
-   echo fmt"info: {file} {f + 1}: line had radionomy link"
-   
-  elif inSeq[f].contains " ":
-   echo fmt"info: {file} {f + 1}: line had space"
-   inSeq[f] = inSeq[f].strip()
-   try:
-    if inSeq[f].find("=") == -1: echo fmt"info: {file} {f + 1}: processed"
-    else:
-     removeSuffix(inSeq[f],"\"")
-     inSeq[f].delete(inSeq.low..(inSeq[f].find("=") + 1))
-   except: discard
-
+ try:
+  var file = lex[f]
+  if fileExists fmt"{file}.bkp": echo fmt"info: {file} aldready processed" ; break
   else:
-   if ( inSeq[f].find "=" ) == -1: echo fmt"info: {file} {f + 1}: processed"
-   else:
-    echo fmt"info: {file} {f + 1}: processed"
-    inSeq[f] = inSeq[f].strip()
-    removeSuffix(inSeq[f],"\"")
-    inSeq[f].delete(inSeq.low..(inSeq[f].find("=") + 1))
+   file.copyFile fmt"{file}.bkp"
+   var inSeq = splitLines readFile file
 
- #echo inSeq.join("\n").strip()
- writeFile(file,inSeq.join("\n"))
+   for f in inSeq.low..inSeq.high:
+    if ( inSeq[f].find "=" ) == -1: echo fmt"info: {file} aldready processed" ; break
+    elif inSeq[f] == "": echo fmt"info: {file} {f + 1}: line is nil"
+    elif inSeq[f].contains "onomy":
+     inSeq[f] = ""
+     inSeq[int( inSeq.high / 2 ) - int( inSeq.high - f )] = ""
+     echo fmt"info: {file} {f + 1}: line had radionomy link"
+    else:
+     inSeq[f] = strip inSeq[f]
+     inSeq[f].removeSuffix "\""
+     inSeq[f].delete(inSeq.low..(inSeq[f].find("=") + 1))
+
+   file.writeFile strip inSeq.join "\n"
+ except IOError,OSError: echo fmt"no such file"
+ except: echo fmt"unexpected error"
