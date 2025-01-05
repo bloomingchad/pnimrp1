2267a15d12d2be5de6fe7e0385a304078062c7e2
Anton <162481656+bloomingchad@users.noreply.github.com>
2025-01-05 15:43:48 +0530

    Update menu.nim

--- src/menu.nim
+++ src/menu.nim
@@ -223,7 +223,7 @@ proc loadCategories*(baseDir = getAppDir() / "assets"): tuple[names, paths: seq[
     result.names.add("Notes")
 
 
-proc handleStationMenu*(section = ""; jsonPath = ""; subsection = "") {.raises: [MenuError].} =
+proc handleStationMenu*(section = ""; jsonPath = ""; subsection = "") =
   ## Handles the station selection menu
   if section.endsWith(DirSep):
     drawMenu("Main", @[], section)
